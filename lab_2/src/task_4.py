e8d40fbfadea3f3393035033d065f5ad263d6f91
Alexey <fisyuk.ayu@edu.spbstu.ru>
2022-04-22 01:24:21 +0300

    Добавил гистограммы

--- lab_2/src/task_4.py
+++ lab_2/src/task_4.py
@@ -16,8 +16,8 @@ class Task4:
     def run(self):
         alpha = 0.05
 
-        m_all = {"norm": list(), "asymp": list()}
-        s_all = {"norm": list(), "asymp": list()}
+        m_dict = {"norm": list(), "asymp": list()}
+        s_dict = {"norm": list(), "asymp": list()}
         x_all = list()
         for n in [20, 100]:
             x = self.normal(n)
@@ -29,23 +29,45 @@ class Task4:
             s_n = [s * np.sqrt(n) / np.sqrt(stats.chi2.ppf(1 - alpha / 2, n - 1)),
                   s * np.sqrt(n) / np.sqrt(stats.chi2.ppf(alpha / 2, n - 1))]
 
-            m_all["norm"].append(m_n)
-            s_all["norm"].append(s_n)
+            m_dict["norm"].append(m_n)
+            s_dict["norm"].append(s_n)
 
             m_as = [m - stats.norm.ppf(1 - alpha / 2) / np.sqrt(n), m + stats.norm.ppf(1 - alpha / 2) / np.sqrt(n)]
             e = (sum(list(map(lambda el: (el - m) ** 4, x))) / n) / s ** 4 - 3
             s_as = [s / np.sqrt(1 + stats.norm.ppf(1 - alpha / 2) * np.sqrt((e + 2) / n)),
                     s / np.sqrt(1 - stats.norm.ppf(1 - alpha / 2) * np.sqrt((e + 2) / n))]
 
-            m_all["asymp"].append(m_as)
-            s_all["asymp"].append(s_as)
+            m_dict["asymp"].append(m_as)
+            s_dict["asymp"].append(s_as)
 
-        for key in m_all.keys():
-            print(f"m {key} : {m_all[key][0]},  {m_all[key][1]}")
-            print(f"sigma {key}: {s_all[key][0]},  {s_all[key][1]}")
+        for key in m_dict.keys():
+            print(f"m {key} : {m_dict[key][0]},  {m_dict[key][1]}")
+            print(f"sigma {key}: {s_dict[key][0]},  {s_dict[key][1]}")
+            self.draw_result(x_all, m_dict[key], s_dict[key], key)
 
+    def draw_result(self, x_set, m_all, s_all, name):
+        fig, (ax1, ax2) = plt.subplots(1,2)
 
+        ax1.set_ylim(0, 1)
+        ax1.set_title(name)
+        ax1.hist(x_set[0], density=True, histtype="stepfilled", label='n = 20')
+        ax1.legend(loc='upper right')
+        ax2.set_title(name)
+        ax2.set_ylim(0, 1)
+        ax2.hist(x_set[1], density=True, histtype="stepfilled", label='n = 100')
+        ax2.legend(loc='upper right')
+        fig.show()
+        fig, (ax3, ax4) = plt.subplots(1,2)
 
+        ax3.set_title(name + ' "m"')
+        ax3.set_ylim(0.9, 1.4)
+        ax3.plot(m_all[0], [1, 1], 'go-', label='n = 20')
+        ax3.plot(m_all[1], [1.1, 1.1], 'mo-', label='n = 100')
+        ax3.legend()
 
-
-
+        ax4.set_title(name + ' "sigma"')
+        ax4.set_ylim(0.9, 1.4)
+        ax4.plot(s_all[0], [1, 1], 'go-', label='n = 20')
+        ax4.plot(s_all[1], [1.1, 1.1], 'mo-', label='n = 100')
+        ax4.legend()
+        fig.show()
